b0eb42863a24b8634158e8ad21e61a9e90610822
RoyAbra27 <97115783+RoyAbra27@users.noreply.github.com>
2023-02-28 18:32:01 +0200

    fixed stack logic and changed skills visuals

--- src/widgets/Stack.jsx
+++ src/widgets/Stack.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
-import { setSkills, toggleStack } from '../store/resumeSlice.js'
+import { setSkills, toggleStack, removeSkill } from '../store/resumeSlice.js'
 import { useDispatch, useSelector } from 'react-redux'
-import { Input, Switch } from 'antd'
+import { Input, Switch, Tag } from 'antd'
+import { useState } from 'react'
 import CustomRow from '../components/shared/CustomRow.jsx'
 
 const StackWidget = () => {
   const dispatch = useDispatch()
   const { display, stack } = useSelector((state) => state.ResumeStore)
+  const [userInput, setUserInput] = useState('')
+
+  const handleOnEnter = () => {
+    userInput.length > 0 && dispatch(setSkills(userInput)), setUserInput('')
+    return
+  }
   return (
     <Container>
       <CustomRow>
@@ -18,14 +25,31 @@ const StackWidget = () => {
         />
       </CustomRow>
       <CustomRow>
-        Enter skills seperated by comma , to make skill bold, just click on it
-        on the resume document!
+        Enter skills here, to make skill bold, just click on it on the resume
+        document!
       </CustomRow>
       <CustomRow>
+        <StackItems>
+          {stack &&
+            stack.map((skill) => (
+              <Tag
+                key={skill.id}
+                closable={true}
+                onClose={() => {
+                  dispatch(removeSkill(skill.id))
+                }}
+              >
+                {skill.name}
+              </Tag>
+            ))}
+        </StackItems>
         <Input
+          placeholder="Add skill"
+          value={userInput}
           disabled={!display.stack}
-          value={stack}
-          onChange={({ target }) => dispatch(setSkills(target.value))}
+          size="small"
+          onChange={({ target }) => setUserInput(target.value)}
+          onPressEnter={handleOnEnter}
         />
       </CustomRow>
     </Container>
@@ -36,6 +60,10 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+`
+const StackItems = styled.p`
+  display: flex;
+  flex-wrap: wrap;
 `
 
 export default StackWidget
