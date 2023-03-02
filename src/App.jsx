936093a3a7853bb47b6bcd24735021df008f7c33
Tal Moskovich <tal.moskovich@gmail.com>
2023-03-02 10:36:09 +0200

    merged with master

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-import React, { useEffect, useRef, useState } from 'react'
-import styled from 'styled-components'
-import GlobalStyle from './constants/GlobalStyle.jsx'
-import Controller from './components/ResumeOptions.jsx'
-import AppHeader from './components/AppHeader.jsx'
-import Renderer from './widgets/Renderer.jsx'
-import { useDispatch, useSelector } from 'react-redux'
-import {
-  forceNarrowHeader,
-  setThemeColor,
-  toggleNarrowHeader,
-  toggleRenderer,
-} from './store/resumeSlice.js'
 import './assets/Square-Regular.otf'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
@@ -21,13 +8,13 @@ import RootLayout from './pages/RootLayout'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <RootLayout />, // <-- main app is here
+    element: <RootLayout />, // <-- main layout is here
     children: [
       { index: true, element: <HomePage /> },
       {
         path: 'cv',
         children: [
-          { index: true, element: <CreationPage /> },
+          { index: true, element: <CreationPage /> }, // <= cv creation is here
           { path: ':id', element: <CreationPage /> },
         ],
       },
