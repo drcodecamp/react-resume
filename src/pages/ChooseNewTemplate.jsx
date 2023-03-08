b1057b2650485756a801a36bcead8798811cccf8
Tal Moskovich <tal.moskovich@gmail.com>
2023-03-08 18:46:23 +0200

    added new choose-template page, moved templates to another file, added to slice tempaltes array

---
+++ src/pages/ChooseNewTemplate.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { addResumeDocument } from '../store/resumeSlice.js'
+
+const ChooseNewTemplate = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const templates = useSelector((state) => state.resume.templates)
+  const handleNewDocumentCreation = (templateId) => {
+    dispatch(addResumeDocument(templateId))
+    navigate('/editor')
+  }
+  return (
+    <>
+      <Container>
+        <Titles>
+          <h1>בחרו את הטמפלייט שלכם</h1>
+          <h2>אל תדאגו, אפשר לשנות הכל גם בהמשך</h2>
+        </Titles>
+        <ButtonsContainer>
+          {templates &&
+            Object.keys(templates).length > 0 &&
+            Object.keys(templates).map((templateId) => {
+              return (
+                <ButtonContainer key={templateId}>
+                  <Button onClick={() => handleNewDocumentCreation(templateId)}>
+                    <img
+                      src={templates[templateId].image}
+                      alt="choose Template"
+                    />
+                    <p>בחירה</p>
+                  </Button>
+                </ButtonContainer>
+              )
+            })}
+        </ButtonsContainer>
+      </Container>
+    </>
+  )
+}
+
+const ButtonsContainer = styled.section`
+  display: flex;
+  align-self: center;
+  flex-wrap: wrap;
+  color: white;
+  padding: 2em;
+  gap: 1.5em;
+  @media screen and (max-width: 1280px) {
+    justify-content: space-evenly;
+  }
+`
+
+const Titles = styled.section`
+  color: white;
+  text-align: center;
+  align-self: center;
+  @media screen and (max-width: 1280px) {
+    font-size: 12px;
+  }
+  h1 {
+    font-size: 2em;
+    font-weight: 700;
+  }
+
+  h2 {
+    color: #c9c9c9;
+    font-size: 1.5em;
+  }
+
+  h3 {
+    color: #c9c9c9;
+    font-size: 2em;
+  }
+`
+
+const Container = styled.div`
+  padding-top: 2em;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  @media screen and (max-width: 1280px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+`
+
+const Button = styled.div`
+  cursor: pointer;
+  position: relative;
+  display: flex;
+  z-index: 10;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.25s ease;
+  img {
+    width: 12em;
+  }
+  :hover {
+    transform: scale(1.05);
+  }
+`
+
+const ButtonContainer = styled.button`
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  p {
+    font-weight: bolder;
+    font-size: 1em;
+    padding: 0.5em;
+    margin-top: 0.5em;
+    background-color: #2b292f;
+    border-radius: 10px;
+    width: 60%;
+    text-align: center;
+    transition: all 0.25s ease;
+    :hover {
+      transform: scale(1.15);
+    }
+  }
+`
+
+export default ChooseNewTemplate
---
+++ src/store/resumeTemplates.js
@@ -0,0 +1,138 @@
+import EDUCATION_ICON from '../assets/education.webp'
+import SRC from '../assets/bg1.webp'
+import SRC2 from '../assets/bg2.webp'
+import SRC3 from '../assets/bg3.webp'
+import DEMO_WORK_ICON from '../assets/work.webp'
+import { getRandomName } from '../constants/names.js'
+import { nanoid } from 'nanoid'
+
+export const defaultTemplate = {
+  documentName: getRandomName(),
+  display: {
+    renderer: true,
+    sideNav: false,
+    education: true,
+    email: false,
+    summary: false,
+    stack: true,
+    projects: true,
+    oneLineProjects: false,
+    experience: true,
+    experienceInFreeText: true,
+    jobIcons: true,
+    educationIcons: true,
+    social: {
+      facebook: false,
+      link: false,
+      github: false,
+      youtube: false,
+      instagram: false,
+      medium: false,
+    },
+  },
+  themeColor: '#0008ff',
+  isDarkMode: false,
+  fullName: 'Doctor Code',
+  phone: '050-510-1952',
+  title: 'Frontend Developer',
+  email: 'info@doctorcode.org',
+  summary:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum nisi sed bibendum venenatis. Cras consequat mollis pretium. Nam quam lacus biam',
+  projects: [
+    {
+      id: nanoid(),
+      image: SRC,
+      name: 'Project Name',
+      info: 'Lorem ipsum dolor sit amet, consecr adipisng elit. Dolor dolore eaque laud ume maxime',
+      codeLink: 'doctorcode.org/',
+      demoLink: 'doctorcode.org/',
+    },
+    {
+      id: nanoid(),
+      image: SRC2,
+      name: 'Project Name',
+      info: 'Lorem ipsum dolor sit amet, consecr adipisng elit. Dolor dolore eaque laud ume maxime',
+      codeLink: 'doctorcode.org/',
+      demoLink: 'doctorcode.org/',
+    },
+    {
+      id: nanoid(),
+      image: SRC3,
+      name: 'Project Name',
+      info: 'Lorem ipsum dolor sit amet, consecr adipisng elit. Dolor dolore eaque laud ume maxime',
+      codeLink: 'doctorcode.org/',
+      demoLink: 'doctorcode.org/',
+    },
+  ],
+  stack: [
+    { id: nanoid(), name: 'javascript', isActivated: true },
+    { id: nanoid(), name: 'typescript', isActivated: false },
+    { id: nanoid(), name: 'react.js', isActivated: true },
+    { id: nanoid(), name: 'angular', isActivated: false },
+    { id: nanoid(), name: 'vue.js', isActivated: false },
+    { id: nanoid(), name: 'node.js', isActivated: true },
+    { id: nanoid(), name: 'express', isActivated: false },
+    { id: nanoid(), name: 'mongoDB', isActivated: false },
+    { id: nanoid(), name: 'micro-services', isActivated: false },
+    { id: nanoid(), name: 'html', isActivated: false },
+    { id: nanoid(), name: 'css', isActivated: false },
+    { id: nanoid(), name: 'sass', isActivated: false },
+    { id: nanoid(), name: 'bootstrap', isActivated: false },
+  ],
+  socialUrls: {
+    facebook: '',
+    linkedin: '',
+    github: '',
+    youtube: '',
+    instagram: '',
+    medium: '',
+  },
+  experience: [
+    {
+      id: nanoid(),
+      icon: DEMO_WORK_ICON,
+      name: 'Demo company',
+      industry: '| Crypto',
+      date: '2 Years',
+      informationList: [
+        {
+          id: nanoid(),
+          val: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proibibendum venenatis',
+        },
+      ],
+      information:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum nisi sed bibendum venenatis. Cras consequat mollis pretium. Nam quam lacus biam.',
+    },
+    {
+      id: nanoid(),
+      icon: DEMO_WORK_ICON,
+      name: 'Demo company',
+      industry: '| Fintech',
+      date: '2 Years',
+      informationList: [
+        {
+          id: nanoid(),
+          val: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proibibendum venenatis',
+        },
+      ],
+      information:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum nisi sed bibendum venenatis. Cras consequat mollis pretium. Nam quam lacus biam.',
+    },
+  ],
+  education: [
+    {
+      id: nanoid(),
+      icon: EDUCATION_ICON,
+      name: 'Youtube @DoctorCode',
+      duration: '3 Months',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rutrum nisi sed bibendum venenatis. Cras consequat mollis pretium. Nam quam lacus biam.',
+    },
+  ],
+  value: 0,
+}
+
+export const oneLineProjectsTemplate = {
+  ...defaultTemplate,
+  display: { ...defaultTemplate.display, oneLineProjects: true },
+}
